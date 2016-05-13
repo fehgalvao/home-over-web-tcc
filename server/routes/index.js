e7dd7cdfdbdd242f101e0c5fbcbe0ca3daeeadd7
Daniel Abrão <dcerag@br.ibm.com>
2016-05-12 21:22:57 -0300

    Project structure

---
+++ server/routes/index.js
@@ -0,0 +1,22 @@
+/*jslint node:true*/
+(function () {
+    'use strict';
+    var db = require('../config/db.js').init,
+        starterRoutes = require('./partials/starter.js');
+    module.exports = function (app) {
+        starterRoutes(app);
+
+        app.get('/', function (req, res) {
+            res.render('index.html');
+        });
+
+        app.get('/404', function (req, res) {
+            res.status(404).render('../../client/handlers/error.view.html', { error: req });
+        });
+
+        app.use(function (req, res) {
+            res.redirect('/404');
+        });
+    };
+
+}());
